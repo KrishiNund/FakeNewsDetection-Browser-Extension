9593a3243517963dd029bbb8bbb62d910d3c9d8a
K_Nund <ON144@live.mdx.ac.uk>
2025-02-27 14:05:00 +0400

    -Changes name of extension

--- browser-extension/src/components/Dashboard.jsx
+++ browser-extension/src/components/Dashboard.jsx
@@ -1,20 +1,16 @@
-// import React, { useState } from "react";
-
-// const [postsAnalysed,setPostsAnalysed] = useState(0)
-
 function Dashboard(){
     return(
-        <div className="flex flex-col w-3xs h-28 bg-[#415A77] items-center justify-center text-lg font-semibold text-[#E0E1DD]">
+        <div className="flex flex-col w-xs h-28 bg-[#415A77] items-center justify-center text-lg font-semibold text-[#E0E1DD]">
             <div className="flex justify-center items-center gap-x-4">
-                <p>Posts Analysed:</p>
+                <p>Total Assessments Made:</p>
                 <p>0</p>
             </div>
             <div className="flex justify-center items-center gap-x-4">
-                <p>Predictions Made:</p>
+                <p>Number of fake news detected:</p>
                 <p>0</p>
             </div>
             <div className="flex justify-center items-center gap-x-4">
-                <p>Predictions reported:</p>
+                <p>Incorrect Assessments Reported:</p>
                 <p>0</p>
             </div>
         </div>  
