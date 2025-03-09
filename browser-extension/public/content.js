f20d34e6840e10034c8c8a514b9ebefb764b94a4
K_Nund <ON144@live.mdx.ac.uk>
2025-03-09 10:40:10 +0400

    - changed feedContainer element's class and the posts element's class  to a more general one so that extension badge can appear on main feed, saved posts and even profile posts

--- browser-extension/public/content.js
+++ browser-extension/public/content.js
@@ -1,40 +1,42 @@
 //extracting text facebook posts currently on the screen
 let extractedPostContent = [];
 function extractTextPosts(){
-    let posts = document.querySelectorAll('div.x1n2onr6.x1ja2u2z'); //[data-ad-rendering-role="meta"] div[data-ad-preview="message"]
+    //getting all post elements
+    let posts = document.querySelectorAll('div.x1a2a7pz');
     posts.forEach(post => {
         //getting the text and url elements from the post
         let textElement = post.querySelector('div[data-ad-preview="message"]');
         let urlElement = post.querySelector('div[data-ad-rendering-role="meta"]');
-        //get close button from the post
-        // let closeButton = post.querySelector('a[aria-label="hide post"]');
+
+        //getting the menu button on the post (the three dots button)
         let menuButton = post.querySelector('div[aria-haspopup="menu"]');
         
         //extracting the text and url from those elements
         let textContent = textElement ? textElement.innerText.trim() : 'No text';
         let urlContent = urlElement ? urlElement.innerText.trim() : 'No URL';
 
-        //only add to array if text length is considerable and text is not duplicate
+        //only add to array if text length is significant and the array does not already contain this text
         if(textContent.length > 20 && !post.querySelector('.lmc-badge')){
             extractedPostContent.push({text:textContent, url:urlContent});
 
-            //create a badge that will contain the credibility assessment
+            //creating a badge that will contain the credibility assessment
             let badge = document.createElement('div');
             badge.innerText = '🔍';
             badge.classList.add("lmc-badge");
 
+            //creating the tooltip that will show the assessment details on hovering the badge
             let tooltip = document.createElement('div');
             tooltip.innerText = `Text: ${textContent}\nURL: ${urlContent}`;
             tooltip.classList.add("lmc-tooltip");
 
             badge.append(tooltip);
 
-            //add badge next to close button
+            //add badge next to menu button
             if (menuButton){
-                console.log("it does exist");
                 menuButton.parentNode.insertBefore(badge, menuButton.nextSibling);
             } else {
-                post.appendChild(badge);
+                console.log("No Menu Button");
+                // post.appendChild(badge);
             }
         }
     });
@@ -47,7 +49,7 @@ function extractTextPosts(){
     
 }
 
-//Mutation observer to detect new posts as user scrolls
+//creating a Mutation observer to detect new posts as the user scrolls
 const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation =>{
         if (mutation.addedNodes.length > 0){
@@ -57,7 +59,7 @@ const observer = new MutationObserver(mutations => {
 })
 
 //start observing the facebook feed
-const feedContainer = document.querySelector('div.x1hc1fzr.x1unhpq9.x6o7n8i');
+const feedContainer = document.querySelector('div.x9f619.x1n2onr6.x1ja2u2z');
 if (feedContainer){
     observer.observe(feedContainer, {childList: true, subtree: true});
 }
