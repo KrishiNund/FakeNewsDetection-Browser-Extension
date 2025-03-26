4c9ae55d05262fabde98a3b09334d1d36b57b659
K_Nund <ON144@live.mdx.ac.uk>
2025-03-26 10:11:52 +0400

    - makes unsupported page prettier

--- frontend/src/components/UnsupportedPage.jsx
+++ frontend/src/components/UnsupportedPage.jsx
@@ -1,7 +1,19 @@
+import {Ban} from "lucide-react";
+
 function UnsupportedPage(){
     return(
-        <div className="w-3xs h-14 bg-[#ffffff] text-center text-lg font-semibold">
-            <p>Unsupported site. Works only on Facebook Pages</p>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="bg-white p-6 shadow-md rounded-lg max-w-md text-center border border-red-300">
+                <div className="flex items-center justify-center text-red-500">
+                    <Ban className="w-8 h-8" />
+                </div>
+                <h2 className="text-lg font-semibold mt-2 text-red-700">
+                    Unsupported Site
+                </h2>
+                <p className="text-sm text-gray-600 mt-1">
+                    This extension only works on Facebook Pages.
+                </p>
+            </div>
         </div>
     )
 }
